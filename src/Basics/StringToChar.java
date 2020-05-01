fb2b6b865b4168367f8d07c300e0fcf60ceb7514
vivekchaubey <vivek.chaubey@iitdalumni.com>
2020-05-01 18:19:46 -0400

    string to character

---
+++ src/Basics/StringToChar.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.*;
+import java.io.*;
+
+public class StringToChar{
+    public static void main(String args[]){
+
+        String testString = "vivek prakash chaubey";
+        char[] stringtochararray = testString.toCharArray();
+        System.out.println(stringtochararray[0]);
+
+    }//main function end
+
+
+}//class end
